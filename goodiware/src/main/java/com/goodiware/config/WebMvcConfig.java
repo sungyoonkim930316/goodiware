cdf0d512c539a005ef7c3385dab4ac3019e080c8
minsuback <minsuback721@gmail.com>
2020-04-21 15:17:32 +0900

    백민수 - 게시판 댓글 수정 기능 구현

--- goodiware/src/main/java/com/goodiware/config/WebMvcConfig.java
+++ goodiware/src/main/java/com/goodiware/config/WebMvcConfig.java
@@ -1,7 +1,13 @@
 package com.goodiware.config;
 
+import java.nio.charset.Charset;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,6 +27,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
 				.excludePathPatterns("/resources/**");
 		
 	}
+	
+//	@Bean
+//	public HttpMessageConverter<String> responseBodyconConverter() {
+//		// @ResponseBody 응답에 대한 encoding 설정
+//		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+//	}
+//	
+//	@Bean
+//	public HiddenHttpMethodFilter methodFilter() {
+//		HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+//		return filter;
+//	}
 	
 	
 }
