68f976c1ff3d406612a314a21fdc414057f61331
hbj3809 <hbj3809@naver.com>
2020-04-24 19:26:29 +0900

    황병준 - redis 안정화 해봤지만... 실패...

--- goodiware/src/main/java/com/goodiware/config/EmbeddedRedisConfig.java
+++ goodiware/src/main/java/com/goodiware/config/EmbeddedRedisConfig.java
@@ -1,38 +1,42 @@
 package com.goodiware.config;
 
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 import redis.embedded.RedisServer;
 
 @Profile("local")
 @Configuration("embeddedRedisConfig")
+@EnableRedisRepositories
 public class EmbeddedRedisConfig {
 
 	@Value("${spring.redis.port}")
 	private int redisPort;
-	
+
 	private RedisServer redisServer;
-	
+
 	@PostConstruct
-	public void redisServer() {
-		
+	public void redisServer() throws IOException {
+
 		redisServer = new RedisServer(redisPort);
 		redisServer.start();
-		
+
 	}
 
 	@PreDestroy
 	public void stopRedis() {
-		
-		if(redisServer != null) {
+
+		if (redisServer != null) {
 			redisServer.stop();
 		}
-		
+
 	}
-	
+
 }
--- goodiware/src/main/java/com/goodiware/config/RedisConfig.java
+++ goodiware/src/main/java/com/goodiware/config/RedisConfig.java
@@ -1,12 +1,15 @@
 package com.goodiware.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,8 +19,18 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
+//new
+@EnableRedisRepositories
+
 public class RedisConfig {
 
+	//new
+	@Value("${spring.redis.host}")
+    private String redisHost;
+	//new
+    @Value("${spring.redis.port}")
+    private int redisPort;
+	
 	// 단일 topic 사용을 위한 Bean설정
 	@Bean
 	public ChannelTopic channelTopic() {
@@ -46,13 +59,24 @@ public class RedisConfig {
 		
 	}
 	
+	//new
+	@Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+	
 	// 앱에서 사용할 레디스템플릿 set
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
 		
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 		
-		redisTemplate.setConnectionFactory(connectionFactory);
+		// 기존 커넥션 팩토리
+		//redisTemplate.setConnectionFactory(connectionFactory);
+		
+		// new
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
 		
